8776ebc4e52f7918370972489589005bfa054c51
Mozart2-ops <randrianivolalainaalpha@gmail.com>
2025-09-10 11:13:17 +0300

    modification : .env et vite.congi.js et cors pour affichage mobile

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,19 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
         tailwindcss(),
     ],
-   server: {
-        host: '0.0.0.0', // Permet l'accès depuis le réseau local
-        port: 5173, // Port par défaut de Vite
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
         strictPort: true,
         hmr: {
-            host: 'localhost',
+            host: '10.0.0.141', // VOTRE IP LOCALE
+            protocol: 'ws'
         },
     },
 });
